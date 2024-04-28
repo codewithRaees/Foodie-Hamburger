1e3c8d11eefccdb9bf1402d45a6dd432279ab8fc
Raees <mraiskundi@gmail.com>
2024-04-28 12:29:50 +0500

    Hamburger Menu step by step

---
+++ main.js
@@ -0,0 +1,17 @@
+// const showMenu = document.querySelector(".hamburger-menu");
+// var navBar = document.querySelector("nav");
+// var closeicon = document.querySelector(".close-icon");
+// // console.log(navBar);
+// const menu = false;
+// navBar.style.display = "none";
+
+// showMenu.addEventListener("click", () => {
+//   if (!menu) {
+//     navBar.style.display = "block";
+//   }
+//   //   showMenu.style.display = "none";
+// });
+// closeicon.addEventListener("click", () => {
+//   navBar.style.display = "none";
+//   showMenu.style.display = "block";
+// });
