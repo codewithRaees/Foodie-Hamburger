27ac62608eba7a314db80baf01496e4317415c4f
Raees <mraiskundi@gmail.com>
2024-04-29 17:09:18 +0500

    Hamburger Menu Step by Step

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
 // const showMenu = document.querySelector(".hamburger-menu");
 // var navBar = document.querySelector("nav");
 // var closeicon = document.querySelector(".close-icon");
+// const hamburgerMenu = document.querySelector(".hamburger-menu-container");
+// console.log(hamburgerMenu);
 // // console.log(navBar);
 // const menu = false;
 // navBar.style.display = "none";
 
 // showMenu.addEventListener("click", () => {
 //   if (!menu) {
-//     navBar.style.display = "block";
+//     navBar.style.display = "flex";
+//     hamburgerMenu.style.display = "none";
+//   } else {
+//     showMenu.style.display = "flex";
+//     hamburgerMenu.style.display = "block";
 //   }
-//   //   showMenu.style.display = "none";
 // });
 // closeicon.addEventListener("click", () => {
 //   navBar.style.display = "none";
-//   showMenu.style.display = "block";
+//   hamburgerMenu.style.display = "block";
 // });
+const showmenu = document.querySelector(".menu-main-container");
+const hamburgerMenu = document.querySelector(".hamburger-menu-container");
+const mainwrapper = document.querySelector(".main-wrapper");
+var navBar = document.querySelector("nav");
+
+var closeicon = document.querySelector(".close-icon");
+var go = document.querySelector("h3");
+
+hamburgerMenu.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  showmenu.classList.add("open-menu");
+});
+
+closeicon.addEventListener("click", (e) => {
+  showmenu.classList.remove("open-menu");
+});
+
+//Go to Top click Listner
+go.addEventListener("click", () => {
+  mainwrapper.scrollTo(0, 0);
+});
+window.addEventListener("click", () => {
+  showmenu.classList.remove("open-menu");
+});
+
+// To apply stoppropagation on nav bar
+
+navBar.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
